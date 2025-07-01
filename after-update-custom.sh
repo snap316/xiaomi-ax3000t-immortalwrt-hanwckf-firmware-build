e84354cba44176052aec922c5995db261da4ba20
snap316 <bks@outlook.in>
2025-07-01 11:08:11 +0400

    Update after-update-custom.sh

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -2,7 +2,7 @@
 # Description: (After Update feeds)
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # Set DHCP IP Address start and end
 sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
